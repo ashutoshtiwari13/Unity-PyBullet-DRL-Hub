90483ee07f4d0c54cf9d1ab64be62b377eeb62c7
ashutoshtiwari13 <ashu4.bms@gmail.com>
2020-09-08 09:29:54 +0530

    Hyperparameters

--- Banana-Env-DQN/dqn_agent.py
+++ Banana-Env-DQN/dqn_agent.py
@@ -7,3 +7,10 @@ from model import QNetwork
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR = 5e-4               # learning rate
+UPDATE_EVERY = 4        # how often to update the network
