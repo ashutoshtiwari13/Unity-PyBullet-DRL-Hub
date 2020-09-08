222d32ea1bade95091b796a28d97fba821fecbac
ashutoshtiwari13 <ashu4.bms@gmail.com>
2020-09-08 21:54:48 +0530

    Model setup :heavy_check_mark:

--- Crawler-Env-PPO/model.py
+++ Crawler-Env-PPO/model.py
@@ -3,6 +3,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
@@ -90,3 +94,35 @@ class Critic(nn.Module):
         x = F.relu(self.fc2a(self.fc2a_bn(x)))
 
         return F.tanh(self.fc3(self.fc3_bn(x)))
+class PPO_Actor_Critic(nn.Module):
+
+#    def __init__(self, state_size, action_size, seed, fc1_units=1024, fc2_units=1024):
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=128):
+        super(PPO_Actor_Critic, self).__init__()
+        self.actor = Actor(state_size, action_size, seed, fc1_units, fc2_units)
+        self.critic = Critic(state_size, seed, fc1_units, fc2_units)
+        self.std = nn.Parameter(torch.ones(1, action_size)*0.15)
+
+    def forward(self, state, action=None, scale=1.):
+        """Build Policy.
+
+        Returns
+        ======
+            action (Tensor): predicted action or inputed action
+            log_prob (Tensor): log probability of current action distribution
+            ent (Tensor): entropy of current action distribution
+            value (Tensor): estimate value function
+        """
+        action_mean = self.actor(state)
+        value = self.critic(state)
+
+        dist = torch.distributions.Normal(action_mean, F.hardtanh(self.std, min_val=0.06*scale, max_val=0.6*scale))
+
+        if action is None:
+            action = dist.sample()
+        log_prob = dist.log_prob(action)
+        log_prob = torch.sum(log_prob, dim=1, keepdim=True)
+
+        ent = dist.entropy().mean()
+
+        return action, log_prob, ent, value
